4769703b050727951cf0a0446a158613749bd18d
Angie Munoz <angiepmunoz@gmail.com>
2024-03-12 13:59:06 -0400

    removed index code to its own component, imported components and rendered on app.js, included css

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,26 @@
-import {useState, useEffect} from "react"
-import axios from "axios"
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Animes from "./components/Animes";
+import Navbar from "./components/Nav";
+import Footer from "./components/Footer";
+import "./App.css";
 
 const API = import.meta.env.VITE_API;
 
 function App() {
-  // const [click, setClick] = useState(1) // useState returns an array that we descructure. The first element is the initial state and the second is the set function 
-
-  // let click =  1;
-  // function handleClick(){
-  //  setClick(click + 1)
-  // }
-
-  //  return <div onClick={handleClick}>Click: {click}</div>
-  const [animes, setAnimes] = useState([]);
-
-  useEffect(()=>{
-      axios.get(`${API}/animes`).then(({data})=>{
-        console.log(data)
-        setAnimes(data.payload)
-      }).catch(e => console.warn("catch:", e))
-  }, [])
   return (
-    <section className="index">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>
-                Name
-              </th>
-              <th>
-                Description
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {animes.map((anime, i)=> {
-               return (
-                <tr key={i}>
-                  <td>{anime.name}</td>
-                  <td>{anime.description}</td>
-                </tr>
-               )
-            })}
-            {/* <tr>
-              <td>anime name</td>
-              <td>anime description</td>
-            </tr>
-            <tr>
-              <td>anime name</td>
-              <td>anime description</td>
-            </tr>
-            <tr>
-              <td>anime name</td>
-              <td>anime description</td>
-            </tr>
-            <tr>
-              <td>anime name</td>
-              <td>anime description</td>
-            </tr> */}
-          </tbody>
-        </table>
-    </section>
-  )
+    <div className="App">
+      <Navbar />
+      <section className="anime-section">
+        <Animes />
+      </section>
+      <Footer/>
+    </div>
+  );
 }
 
-export default App
+export default App;
 
 // Our components will always start off with a capital letter
 // when we use function declarations or arrow functions these are called functional components
-// can only return one parent element 
+// can only return one parent element
